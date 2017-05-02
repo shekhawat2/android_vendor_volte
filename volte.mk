7daeb0f55ac692f4cd1302a315f3c372315e5e67
Ritesh Saxena <riteshsax007@gmail.com>
2017-05-02 08:34:26 +0000

    Fix Wi-Fi Calling

--- volte.mk
+++ volte.mk
@@ -31,7 +31,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.volte_enalbed_by_hw=1 \
     persist.dbg.volte_avail_ovr=1 \
     persist.dbg.vt_avail_ovr=1 \
-    persist.dbg.wfc_avail_ovr=1
+    persist.dbg.wfc_avail_ovr=1 \
+    persist.data.iwlan.enable=true
 
 # libshims
 PRODUCT_PACKAGES += \
